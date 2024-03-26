f505323ceec72225ce97a2561f248931fb1c909a
Marco-Villanueva-21 <159150083+Marco-Villanueva-21@users.noreply.github.com>
2024-03-26 01:24:19 +0000

    fixed python code

--- assignment.py
+++ assignment.py
@@ -1,22 +1,25 @@
 import math
 x = input("Input a diameter. ")
 
-if type(x) == str:
-  print("Error! invalid input (did you input a word?)")
-x = float(x)
+if x.isdigit():
 
-if x > 0:
-  r = x/2
+  x = float(x)
+  if x > 0:
+    r = x/2
 
-  b = r*r
-  A = b*(math.pi)
-  A = round(A, 2)
-  print ("The area is", A)
+    b = r*r
+    A = b*(math.pi)
+    A = round(A, 2)
+    print ("The area is", A)
 
-  y = 2*(math.pi)
-  C = y*r
-  C = round(C, 2)
-  print ("The circumferece is", C)
+    y = 2*(math.pi)
+    C = y*r
+    C = round(C, 2)
 
-elif x <= 0:
-  print("Error! Invalid input (did you input a negative number?)")
+    print ("The circumferece is", C)
+
+  elif x <= 0:
+    print("Error! Invalid input (did you input a negative number?)")
+
+else: 
+  print("Error! Invalid input (did you input a letter?)")
