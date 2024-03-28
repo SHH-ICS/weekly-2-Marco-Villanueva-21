e6ce5b96b9ef798ceeee46dd18a2fc1a6f724046
Marco-Villanueva-21 <159150083+Marco-Villanueva-21@users.noreply.github.com>
2024-03-28 13:30:14 +0000

    finished p[ython code

--- assignment.py
+++ assignment.py
@@ -1,51 +1,28 @@
 import math
-x = input()
+x = ""
 
-if x.find(".") == 1 or x.find(".") == 2 or x.isnumeric():
-  print("recognized number")
-else:
-  print("nt")
-  
-# if x.isnumeric:
-#   print("recognized number")
-# else:
-#   print("nt")
-#   exit()
-# # while x.isdigit() == False:
+while x.isdigit() == False:
 
-#   x = input("Input a diameter. ")
+  x = input("Input a diameter. ")
 
-#   if x.find("-"): 
-#     print("negative number")
-#   else:
+  if x.isnumeric() and x.find("-") ==0 or x.replace(".", "").isdigit():
+    break
+  else:
+    print("Invalid input! Did you input a negative number or letter?")
 
-#   # def is_float(x):
-#   #   if x.replace(".", "").isnumeric():
-#   #     return True
-#   #   else:
-#   #     return False
-        
-# #  if x.isdigit():
-# #    continue
-# #  else: 
-# #    print("Error! Invalid input (did you input a letter or a negative number?)")
+x = float(x)
+if x > 0:
+  r = x/2
 
-    # if x.isdigit():
-    #   x = float(x)
-    #   if x > 0:
-    #     r = x/2
+  b = r*r
+  A = b*(math.pi)
+  A = round(A, 2)
+  print ("The area is", A)
 
-    #     b = r*r
-    #     A = b*(math.pi)
-    #     A = round(A, 2)
-    #     print ("The area is", A)
-
-    #     y = 2*(math.pi)
-    #     C = y*r
-    #     C = round(C, 2)
-
-    #     print ("The circumferece is", C)
-
-    #   elif x <= 0:
-    #     print("Error! Invalid input (did you input a negative number?)")
+  y = 2*(math.pi)
+  C = y*r
+  C = round(C, 2)
+  print ("The circumferece is", C)
+else: 
+ print("Error! Invalid input (did you input a letter or a negative number?)")
     
