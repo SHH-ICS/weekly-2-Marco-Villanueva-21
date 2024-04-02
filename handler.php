d2b11ee8dfdd51045521840d1d9af8af638f3aa1
Marco-Villanueva-21 <159150083+Marco-Villanueva-21@users.noreply.github.com>
2024-04-02 12:52:27 +0000

    changed echos in php file

--- handler.php
+++ handler.php
@@ -4,11 +4,13 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <style
     <title>Calculated!</title>
   </head>
 
   <body>
     
+    <h1>Calculated!<h1>
     <?php
     $rad = "";
     if ( isset( $_POST['rad'] ) ){
@@ -20,16 +22,16 @@
       $b = ($r*$r);
       $A = ($b*pi());
       $A = (round ($A, 2));
-      echo "The area is $A";
+      echo "<p>The area is " .$A. "</p>/n";
  
       $y = (2*pi());
       $C = ($y*$r);
       $C = (round($C, 2));  
-      echo "The circumference is $C";
+      echo "<p>The circumference is " .$C. "</p>\n";
 
     }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$v."</p>\n";
+    // echo "<h1>My Program</h1>\n";
+    // echo "<p>My Variable is = ".$v."</p>\n";
 
     if ($rad<=0) {
       echo "Error! Input invalid! Did you enter a 0 or a negative number?";
